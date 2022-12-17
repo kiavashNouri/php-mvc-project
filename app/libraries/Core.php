d7b945729ae9001226c5049330a1915ca4ffcd16
kiavash nouri <kiavashnouri94@gmail.com>
2022-12-17 12:09:39 +0330

    we get the url at Core and sanitize and exploded this url

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,1 +1,20 @@
 <?php
+class Core{
+    protected $currentController="Home";
+    protected $currentCMethode="index";
+    protected $params=[];
+
+    public function __construct()
+    {
+        $url=$this->getUrl();
+
+
+    }
+    public function getUrl()
+    {
+
+        $url=rtrim($_GET['url'],'/');
+        $url=filter_var($url,FILTER_SANITIZE_URL);
+        return explode("/",$url); //product/index/10  => [0]=>product,[1]=>index [2]=>10
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,2 +1,4 @@
 <?php
 require_once "../app/Bootstrap.php";
+$t=new Core();
+var_dump($t);
