3334962b2dfcd323edfecd8d32e39205e1e1fbae
kiavash nouri <kiavashnouri94@gmail.com>
2022-12-17 12:46:37 +0330

    at Core we checked if Controller exists come and new this

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -7,7 +7,14 @@ class Core{
     public function __construct()
     {
         $url=$this->getUrl();
-
+        if (file_exists("../app/controllers/".ucwords($url[0]).".php")) {
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }else{
+            echo "<h2 style='width: 50%; margin: 0 auto'>Not Found Controller (404)</h2>";
+        }
+        require_once "../app/controllers/".$this->currentController.".php";
+        $this->currentController=new $this->currentController(); //Category=new Category()
 
     }
     public function getUrl()
--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
 <?php
 require_once "../app/Bootstrap.php";
 $t=new Core();
-var_dump($t);
